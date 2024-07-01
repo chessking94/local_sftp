1e79205749299f67894519db01bf4c3d1c821424
Ethan Hunt <chessking94@gmail.com>
2024-06-30 20:40:18 -0500

    Only check directories if user is active

--- src/NotifySFTP.py
+++ src/NotifySFTP.py
@@ -12,14 +12,16 @@ CONFIG_FILE = os.path.join(Path(__file__).parents[1], 'config.json')
 
 
 def get_last_reviewed_timestamp(engine, ftp_user):
-    qry = f"SELECT LastMonitored FROM sftp.Logins WHERE Username = '{ftp_user}'"
+    date_val = None
+    qry = f"SELECT Active, LastMonitored FROM sftp.Logins WHERE Username = '{ftp_user}'"
     logging.debug(qry)
     df = pd.read_sql(qry, engine)
     if len(df) == 0:
         logging.critical(f"unable to locate sftp.Logins record for login '{ftp_user}'")
     else:
-        dte = df.values[0][0]
-        date_val = pd.to_datetime(dte).to_pydatetime()
+        if df.values[0][0]:
+            dte = df.values[0][1]
+            date_val = pd.to_datetime(dte).to_pydatetime()
 
     return date_val
 
@@ -95,55 +97,58 @@ def main():
     for ftp_user in dir_list:
         user_dir = os.path.join(ftp_root, ftp_user)
         last_reviewed = get_last_reviewed_timestamp(engine, ftp_user)
-        new_reviewed = dt.datetime.now()
+        if last_reviewed is None:
+            pass  # user is inactive, do not need to check
+        else:
+            new_reviewed = dt.datetime.now()
 
-        # incoming files to the SFTP server
-        incoming_dir = os.path.join(user_dir, incoming_name)
-        incoming_files = [
-            f for f in os.listdir(incoming_dir)
-            if os.path.isfile(os.path.join(incoming_dir, f))
-            and dt.datetime.fromtimestamp(os.path.getmtime(os.path.join(incoming_dir, f))) > last_reviewed
-        ]
-        incoming_file_ct = len(incoming_files)
-        if incoming_file_ct > 0:
-            msg = f'New SFTP Files: A total of {incoming_file_ct} new file(s) have been uploaded to the HuntHome SFTP by user {ftp_user}'
-            url = f'https://api.telegram.org/bot{tg_api_key}'
-            params = {'chat_id': tg_id, 'text': msg}
-            with requests.post(url + '/sendMessage', params=params) as resp:
-                cde = resp.status_code
-                if cde == 200:
-                    for f in incoming_files:
-                        insert_sftpfiles(engine=engine, username=ftp_user, directory=incoming_name, filename=f)
-                else:
-                    logging.error(f'Incoming File Telegram Notification Failed: Response Code {cde}')
-
-        # outgoing files to the SFTP server
-        outgoing_dir = os.path.join(user_dir, outgoing_name)
-        outgoing_files = [
-            f for f in os.listdir(outgoing_dir)
-            if os.path.isfile(os.path.join(outgoing_dir, f))
-            and dt.datetime.fromtimestamp(os.path.getmtime(os.path.join(outgoing_dir, f))) > last_reviewed
-        ]
-        outgoing_file_ct = len(outgoing_files)
-        if outgoing_file_ct > 0:
-            user_chat_id = get_telegramid(engine=engine, username=ftp_user)
-            if user_chat_id is not None:
-                msg = f'SFTP Files: A total of {outgoing_file_ct} new file(s) are available for download on the HuntHome SFTP and will accessible for {archive_days} days'
+            # incoming files to the SFTP server
+            incoming_dir = os.path.join(user_dir, incoming_name)
+            incoming_files = [
+                f for f in os.listdir(incoming_dir)
+                if os.path.isfile(os.path.join(incoming_dir, f))
+                and dt.datetime.fromtimestamp(os.path.getmtime(os.path.join(incoming_dir, f))) > last_reviewed
+            ]
+            incoming_file_ct = len(incoming_files)
+            if incoming_file_ct > 0:
+                msg = f'New SFTP Files: A total of {incoming_file_ct} new file(s) have been uploaded to the HuntHome SFTP by user {ftp_user}'
                 url = f'https://api.telegram.org/bot{tg_api_key}'
-                params = {'chat_id': user_chat_id, 'text': msg}
+                params = {'chat_id': tg_id, 'text': msg}
                 with requests.post(url + '/sendMessage', params=params) as resp:
                     cde = resp.status_code
                     if cde == 200:
-                        for f in outgoing_files:
-                            insert_sftpfiles(engine=engine, username=ftp_user, directory=outgoing_name, filename=f)
+                        for f in incoming_files:
+                            insert_sftpfiles(engine=engine, username=ftp_user, directory=incoming_name, filename=f)
                     else:
-                        logging.error(f'Outgoing File Telegram Notification Failed: Response Code {cde}')
-            else:
-                for f in outgoing_files:
-                    insert_sftpfiles(engine=engine, username=ftp_user, directory=outgoing_name, filename=f)
+                        logging.error(f'Incoming File Telegram Notification Failed: Response Code {cde}')
 
-        # update database
-        set_last_reviewed_timestamp(engine, ftp_user, new_reviewed)
+            # outgoing files to the SFTP server
+            outgoing_dir = os.path.join(user_dir, outgoing_name)
+            outgoing_files = [
+                f for f in os.listdir(outgoing_dir)
+                if os.path.isfile(os.path.join(outgoing_dir, f))
+                and dt.datetime.fromtimestamp(os.path.getmtime(os.path.join(outgoing_dir, f))) > last_reviewed
+            ]
+            outgoing_file_ct = len(outgoing_files)
+            if outgoing_file_ct > 0:
+                user_chat_id = get_telegramid(engine=engine, username=ftp_user)
+                if user_chat_id is not None:
+                    msg = f'SFTP Files: A total of {outgoing_file_ct} new file(s) are available for download on the HuntHome SFTP and will accessible for {archive_days} days'
+                    url = f'https://api.telegram.org/bot{tg_api_key}'
+                    params = {'chat_id': user_chat_id, 'text': msg}
+                    with requests.post(url + '/sendMessage', params=params) as resp:
+                        cde = resp.status_code
+                        if cde == 200:
+                            for f in outgoing_files:
+                                insert_sftpfiles(engine=engine, username=ftp_user, directory=outgoing_name, filename=f)
+                        else:
+                            logging.error(f'Outgoing File Telegram Notification Failed: Response Code {cde}')
+                else:
+                    for f in outgoing_files:
+                        insert_sftpfiles(engine=engine, username=ftp_user, directory=outgoing_name, filename=f)
+
+            # update database
+            set_last_reviewed_timestamp(engine, ftp_user, new_reviewed)
 
     engine.dispose()
 
